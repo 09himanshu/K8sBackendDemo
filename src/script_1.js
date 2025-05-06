bfe5d1551299a592336938e223aa8689693dd89c
Himanshu Sah <09himanshusah@gmail.com>
2025-05-06 10:32:23 +0530

    add scripts for load testing

---
+++ src/script_1.js
@@ -0,0 +1,8 @@
+function fetchData() {
+  fetch('http://192.168.49.2:30001/getData')
+    .then(response => response.json())
+    .then(data => console.log('Data received:', data))
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+setInterval(fetchData, 300);
