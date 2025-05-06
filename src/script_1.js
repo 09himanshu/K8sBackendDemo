a00401143103934262364b12eef0fcf283f0c69d
Himanshu <himanshu.sah@delostylestudio.com>
2025-05-06 17:29:13 +0530

    small changes

--- src/script_1.js
+++ src/script_1.js
@@ -1,8 +1,22 @@
-function fetchData() {
-  fetch('http://192.168.49.2:30001/getData')
-    .then(response => response.json())
-    .then(data => console.log('Data received:', data))
-    .catch(error => console.error('Error fetching data:', error));
+// function fetchData() {
+//   fetch('http://192.168.49.2:30001/getData')
+//     .then(response => response.json())
+//     .then(data => console.log('Data received:', data))
+//     .catch(error => console.error('Error fetching data:', error));
+// }
+
+// setInterval(fetchData, 300);
+
+async function runInfiniteLoop() {
+  while (true) {
+    try {
+      const response = await fetch('http://192.168.49.2:30001/getData');
+      const data = await response.json();
+      console.log('Data received:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 }
 
-setInterval(fetchData, 300);
+runInfiniteLoop();
